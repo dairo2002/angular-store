9a58bbd8888e59bbc4f43da3c56837b26eece454
dairo santana <dairosantab@gmail.com>
2024-05-14 16:43:13 -0500

    se subio el proyecto al repositorio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './domains/products/pages/list/list.component';
+import { ProductComponent } from './domains/products/components/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Router, Routes } from "@angular/router";
+import { ListComponent } from "./domains/products/pages/list/list.component";
+
+export const routes: Routes = [
+    {
+        path: "", component: ListComponent
+    }
+]
